990fa758d266983cb2eb0ef501a5de9664d0f0eb
Carlos Pio Reyes <alu0101132945@ull.edu.es>
2022-03-17 10:25:39 +0000

    la clase pasa la primera expectativa

---
+++ src/ejercicio-PE103.ts
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+export class Hexadecimal {
+    private value:number;
+    //private hex:string;
+    /**
+     * 
+     * @param val 
+     */
+    constructor(val:number){
+        this.value = val;
+        //this.hex = this.hexa(val)
+    }   
+}
