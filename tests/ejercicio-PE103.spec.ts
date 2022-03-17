81c4c284d38b151b2d7dc45e21a7603b03a6d086
Carlos Pio Reyes <alu0101132945@ull.edu.es>
2022-03-17 10:32:17 +0000

    añadidas las expectativas para los metodos toString y valueOf

--- tests/ejercicio-PE103.spec.ts
+++ tests/ejercicio-PE103.spec.ts
@@ -2,11 +2,18 @@ import 'mocha';
 import {expect} from 'chai';
 import {Hexadecimal} from '../src/ejercicio-PE103';
 
-describe('ej1 function tests', () => {
+describe('ej-PE103 class tests', () => {
   let oneHexadecimal = new Hexadecimal(23)
   let twoHexadecimal = new Hexadecimal(15)
+  let testHexadecimal = new Hexadecimal(38)
    it('Hexadecimal is created', () => {
      expect(oneHexadecimal).to.not.equal(null);
      expect(twoHexadecimal).to.not.equal(null);
    });
+   it('toString() works', () => {
+    expect(testHexadecimal.toString()).to.be.equal('0x26');
+  });
+  it('valueOf() works', () => {
+    expect(testHexadecimal.toString()).to.be.equal(38);
+  });
 });
